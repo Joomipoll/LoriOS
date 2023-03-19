28029ce775342b985b859f2aa1673d0a72a582cc
Joomipoll Deulvart <95223648+Joomipoll@users.noreply.github.com>
2023-03-19 15:11:35 +0700

    Update KernelObj.cpp

--- Kernel/src/obj/KernelObj.cpp
+++ Kernel/src/obj/KernelObj.cpp
@@ -1,4 +1,4 @@
-#include <Objects/KernelObj.h> 
+#include <obj/KernelObj.h> 
 
 void KernelObject::Watch(KernelObjectWatcher& watcher, int events){ watcher.Signal(); }
 void KernelObject::Unwatch(KernelObjectWatcher& watcher){ (void)watcher; }
