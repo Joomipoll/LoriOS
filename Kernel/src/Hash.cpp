93949b830cc1b2f9a2d70c6587590842acb23fc0
Joomipoll Deulvart <95223648+Joomipoll@users.noreply.github.com>
2023-03-21 22:44:07 +0700

    Update Hash.cpp

--- Kernel/src/Hash.cpp
+++ Kernel/src/Hash.cpp
@@ -1,48 +1,51 @@
 #include <Hash.h>
-
 #include <StringView.h>
 #include <String.h>
 
 template<>
-unsigned Hash<unsigned long>(const unsigned long& value){
+unsigned Hash<unsigned long>(const unsigned long& value)
+{
 	return HashU(value);
 }
 
 template<>
-unsigned Hash<unsigned int>(const unsigned int& value){
+unsigned Hash<unsigned int>(const unsigned int& value)
+{
 	return HashU(value);
 }
 
 template<>
-unsigned Hash<unsigned short>(const unsigned short& value){
+unsigned Hash<unsigned short>(const unsigned short& value)
+{
 	return HashU(value);
 }
 
 template<>
-unsigned Hash<unsigned char>(const unsigned char& value){
+unsigned Hash<unsigned char>(const unsigned char& value)
+{
 	return HashU(value);
 }
 
 template<>
-unsigned Hash<StringView>(const StringView& sv){
+unsigned Hash<StringView>(const StringView& sv)
+{
     const char* str = sv.Data();
     unsigned value = (*str << 1);
 
-    while(char c = *str++){
+    while(char c = *str++)
         value ^= HashU(c);
-    }
 
     return value;
 }
 
 template<>
-unsigned Hash<String>(const String& s) {
+unsigned Hash<String>(const String& s)
+{
     const char* str = s.c_str();
     unsigned value = (*str << 1);
 
-    while(char c = *str++){
+    while(char c = *str++)
         value ^= HashU(c);
-    }
 
     return value;
 }
