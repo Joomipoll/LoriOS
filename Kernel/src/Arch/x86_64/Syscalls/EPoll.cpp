53f16990ea7a9d7e73a2ad52de8df3aaafad4c84
Joomipoll Deulvart <95223648+Joomipoll@users.noreply.github.com>
2023-03-21 16:03:57 +0700

    Update EPoll.cpp

--- Kernel/src/Arch/x86_64/Syscalls/EPoll.cpp
+++ Kernel/src/Arch/x86_64/Syscalls/EPoll.cpp
@@ -5,11 +5,13 @@
 #include <UserPointer.h>
 #include <OnCleanup.h>
 
-long SysEpollCreate(RegisterContext* r) {
+long SysEpollCreate(RegisterContext* r)
+{
     Process* proc = Process::Current();
     int flags = SC_ARG0(r);
 
-    if (flags & (~EPOLL_CLOEXEC)) {
+    if (flags & (~EPOLL_CLOEXEC))
+    {
         return -EINVAL;
     }
 
@@ -17,7 +19,8 @@ long SysEpollCreate(RegisterContext* r) {
     FancyRefPtr<UNIXOpenFile> handle = SC_TRY_OR_ERROR(ep->Open(0));
     handle->node = ep;
     handle->mode = 0;
-    if (flags & EPOLL_CLOEXEC) {
+    if (flags & EPOLL_CLOEXEC)
+    {
         handle->mode |= O_CLOEXEC;
     }
 
@@ -25,7 +28,8 @@ long SysEpollCreate(RegisterContext* r) {
     return fd;
 }
 
-long SysEPollCtl(RegisterContext* r) {
+long SysEPollCtl(RegisterContext* r)
+{
     Process* proc = Process::Current();
 
     int epfd = SC_ARG0(r);
@@ -36,7 +40,8 @@ long SysEPollCtl(RegisterContext* r) {
     auto epHandle = SC_TRY_OR_ERROR(proc->GetHandleAs<UNIXOpenFile>(epfd));
     auto handle = SC_TRY_OR_ERROR(proc->GetHandleAs<UNIXOpenFile>(fd));
 
-    if (!(handle && epHandle)) {
+    if (!(handle && epHandle))
+    {
         return -EBADF; // epfd or fd is invalid
     }
 
@@ -44,10 +49,12 @@ long SysEPollCtl(RegisterContext* r) {
         Log::Warning("SysEPollCtl: Currently watching other epoll devices is not supported.");
     }
 
-    if (epfd == fd) {
+    if (epfd == fd)
+    {
         Log::Debug(debugLevelSyscalls, DebugLevelVerbose, "epfd == fd");
         return -EINVAL;
-    } else if (!epHandle->node->IsEPoll()) {
+    } else if (!epHandle->node->IsEPoll())
+    {
         Log::Debug(debugLevelSyscalls, DebugLevelVerbose, "Not an epoll device!");
         return -EINVAL; // Not an epoll device
     }
@@ -56,29 +63,36 @@ long SysEPollCtl(RegisterContext* r) {
 
     ScopedSpinLock<true> lockEp(epoll->epLock);
 
-    if (op == EPOLL_CTL_ADD) {
+    if (op == EPOLL_CTL_ADD)
+    {
         struct epoll_event e;
         TRY_GET_UMODE_VALUE(event, e);
 
-        if (e.events & EPOLLEXCLUSIVE) {
+        if (e.events & EPOLLEXCLUSIVE)
+        {
             // Unsupported
             Log::Warning("SysEPollCtl: EPOLLEXCLUSIVE unsupported!");
         }
 
-        if (e.events & EPOLLET) {
+        if (e.events & EPOLLET)
+        {
             Log::Warning("SysEPollCtl: EPOLLET unsupported!");
         }
 
-        for (const auto& pair : epoll->fds) {
-            if (pair.item1 == fd) {
+        for (const auto& pair : epoll->fds)
+        {
+            if (pair.item1 == fd)
+            {
                 return -EEXIST; // Already watching the fd
             }
         }
 
         epoll->fds.add_back({fd, {e.events, e.data}});
     } else if (op == EPOLL_CTL_DEL) {
-        for (auto it = epoll->fds.begin(); it != epoll->fds.end(); it++) {
-            if (it->item1 == fd) {
+        for (auto it = epoll->fds.begin(); it != epoll->fds.end(); it++)
+        {
+            if (it->item1 == fd)
+            {
                 epoll->fds.remove(it);
                 return 0;
             }
@@ -90,21 +104,25 @@ long SysEPollCtl(RegisterContext* r) {
         struct epoll_event e;
         TRY_GET_UMODE_VALUE(event, e);
 
-        if (e.events & EPOLLEXCLUSIVE) {
+        if (e.events & EPOLLEXCLUSIVE)
+        {
             // Not that we support this flag yet,
             // however for future it can only be
             // enabled on EPOLL_CTL_ADD.
             return -EINVAL;
         }
 
-        for (auto& pair : epoll->fds) {
-            if (pair.item1 == fd) {
+        for (auto& pair : epoll->fds)
+        {
+            if (pair.item1 == fd)
+            {
                 current = &pair.item2;
                 break;
             }
         }
 
-        if (!current) {
+        if (!current)
+        {
             return -ENOENT; // fd not found
         }
     }
@@ -112,7 +130,8 @@ long SysEPollCtl(RegisterContext* r) {
     return -EINVAL;
 }
 
-long SysEpollWait(RegisterContext* r) {
+long SysEpollWait(RegisterContext* r)
+{
     Process* proc = Process::Current();
     Thread* thread = Thread::Current();
 
@@ -122,14 +141,16 @@ long SysEpollWait(RegisterContext* r) {
     long timeout = SC_ARG3(r) * 1000;
 
     auto epHandle = SC_TRY_OR_ERROR(proc->GetHandleAs<UNIXOpenFile>(epfd));
-    if (!epHandle) {
+    if (!epHandle)
+    {
         return -EBADF;
     } else if (!epHandle->node->IsEPoll()) {
         Log::Debug(debugLevelSyscalls, DebugLevelVerbose, "Not an epoll device!");
         return -EINVAL; // Not an epoll device
     }
 
-    if (maxevents <= 0) {
+    if (maxevents <= 0)
+    {
         Log::Debug(debugLevelSyscalls, DebugLevelVerbose, "maxevents cannot be <= 0");
         return -EINVAL; // maxevents cannot be <= 0
     }
@@ -138,13 +159,15 @@ long SysEpollWait(RegisterContext* r) {
 
     int64_t sigmask = SC_ARG4(r);
     int64_t oldsigmask = 0;
-    if (sigmask) {
+    if (sigmask)
+    {
         oldsigmask = thread->signalMask;
         thread->signalMask = sigmask;
     }
 
     OnCleanup([sigmask, oldsigmask, thread]() {
-        if (sigmask) {
+        if (sigmask)
+        {
             thread->signalMask = oldsigmask;
         }
     });
@@ -152,26 +175,34 @@ long SysEpollWait(RegisterContext* r) {
     ScopedSpinLock lockEp(epoll->epLock);
 
     Vector<int> removeFds; // Fds to unwatch
-    auto getEvents = [removeFds](FancyRefPtr<UNIXOpenFile>& handle, uint32_t requested, int& evCount) -> uint32_t {
+    auto getEvents = [removeFds](FancyRefPtr<UNIXOpenFile>& handle, uint32_t requested, int& evCount) -> uint32_t
+    {
         uint32_t ev = 0;
-        if (requested & EPOLLIN) {
-            if (handle->node->CanRead()) {
+        if (requested & EPOLLIN)
+        {
+            if (handle->node->CanRead())
+            {
                 ev |= EPOLLIN;
             }
         }
 
-        if (requested & EPOLLOUT) {
-            if (handle->node->CanWrite()) {
+        if (requested & EPOLLOUT)
+        {
+            if (handle->node->CanWrite())
+            {
                 ev |= EPOLLOUT;
             }
         }
 
-        if (handle->node->IsSocket()) {
-            if (!((Socket*)handle->node)->IsConnected() && !((Socket*)handle->node)->IsListening()) {
+        if (handle->node->IsSocket())
+        {
+            if (!((Socket*)handle->node)->IsConnected() && !((Socket*)handle->node)->IsListening())
+            {
                 ev |= EPOLLHUP;
             }
 
-            if (((Socket*)handle->node)->PendingConnections() && (requested & EPOLLIN)) {
+            if (((Socket*)handle->node)->PendingConnections() && (requested & EPOLLIN))
+            {
                 ev |= EPOLLIN;
             }
         }
@@ -212,7 +243,8 @@ long SysEpollWait(RegisterContext* r) {
         return evs;
     };
 
-    struct EPollFD {
+    struct EPollFD
+    {
         FancyRefPtr<UNIXOpenFile> handle;
         int fd;
         struct epoll_event ev;
@@ -220,15 +252,18 @@ long SysEpollWait(RegisterContext* r) {
 
     Vector<EPollFD> files;
     int evCount = 0; // Amount of fds with events
-    for (const auto& pair : epoll->fds) {
+    for (const auto& pair : epoll->fds)
+    {
         auto result = proc->GetHandleAs<UNIXOpenFile>(pair.item1);
-        if (result.HasError()) {
+        if (result.HasError())
+        {
             continue; // Ignore closed fds
         }
 
         auto handle = std::move(result.Value());
 
-        if (uint32_t ev = getEvents(handle, pair.item2.events, evCount); ev) {
+        if (uint32_t ev = getEvents(handle, pair.item2.events, evCount); ev)
+        {
             if (events.StoreValue(evCount - 1, {
                                                    .events = ev,
                                                    .data = pair.item2.data,
@@ -237,8 +272,10 @@ long SysEpollWait(RegisterContext* r) {
             }
         }
 
-        if (evCount > 0) {
-            if (pair.item2.events & EPOLLONESHOT) {
+        if (evCount > 0)
+        {
+            if (pair.item2.events & EPOLLONESHOT)
+            {
                 removeFds.add_back(pair.item1);
             }
         } else {
@@ -246,24 +283,31 @@ long SysEpollWait(RegisterContext* r) {
                 {handle, pair.item1, pair.item2}); // We only need the handle for later if events were not found
         }
 
-        if (evCount >= maxevents) {
+        if (evCount >= maxevents)
+        {
             goto done; // Reached max events/fds
         }
     }
 
-    if (evCount > 0) {
+    if (evCount > 0)
+    {
         goto done;
     }
 
-    if (!evCount && timeout) {
+    if (!evCount && timeout)
+    {
         FilesystemWatcher fsWatcher;
-        for (auto& file : files) {
+        for (auto& file : files)
+        {
             fsWatcher.WatchNode(file.handle->node, epollToPollEvents(file.ev.events));
         }
 
-        while (!evCount) {
-            if (timeout > 0) {
-                if (fsWatcher.WaitTimeout(timeout)) {
+        while (!evCount)
+        {
+            if (timeout > 0)
+            {
+                if (fsWatcher.WaitTimeout(timeout))
+                {
                     return -EINTR; // Interrupted
                 } else if (timeout <= 0) {
                     return 0; // Timed out
@@ -272,8 +316,10 @@ long SysEpollWait(RegisterContext* r) {
                 return -EINTR; // Interrupted
             }
 
-            for (auto& handle : files) {
-                if (uint32_t ev = getEvents(handle.handle, handle.ev.events, evCount); ev) {
+            for (auto& handle : files)
+            {
+                if (uint32_t ev = getEvents(handle.handle, handle.ev.events, evCount); ev)
+                {
                     if (events.StoreValue(evCount - 1, {
                                                            .events = ev,
                                                            .data = handle.ev.data,
@@ -282,13 +328,16 @@ long SysEpollWait(RegisterContext* r) {
                     }
                 }
 
-                if (evCount > 0) {
-                    if (handle.ev.events & EPOLLONESHOT) {
+                if (evCount > 0)
+                {
+                    if (handle.ev.events & EPOLLONESHOT)
+                    {
                         removeFds.add_back(handle.fd);
                     }
                 }
 
-                if (evCount >= maxevents) {
+                if (evCount >= maxevents)
+                {
                     goto done; // Reached max events/fds
                 }
             }
@@ -296,9 +345,12 @@ long SysEpollWait(RegisterContext* r) {
     }
 
 done:
-    for (int fd : removeFds) {
-        for (auto it = epoll->fds.begin(); it != epoll->fds.end(); it++) {
-            if (it->item1 == fd) {
+    for (int fd : removeFds)
+    {
+        for (auto it = epoll->fds.begin(); it != epoll->fds.end(); it++)
+        {
+            if (it->item1 == fd)
+            {
                 epoll->fds.remove(it);
                 break;
             }
