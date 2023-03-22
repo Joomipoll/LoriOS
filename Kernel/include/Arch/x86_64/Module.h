7d0269210428f84bc34abdc15693ec5b2ac07876
Joomipoll Deulvart <95223648+Joomipoll@users.noreply.github.com>
2023-03-22 21:41:48 +0700

    Create Module.h

---
+++ Kernel/include/Arch/x86_64/Module.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define MODULE_INFO_SYMBOL _moduleInfo
+
+struct LemonModuleInfo
+{
+    const char* name;
+    const char* description;
+
+    int (*init)(void);
+    int (*exit)(void);
+};
+
+#define DECLARE_MODULE(mname, mdescription, minit, mexit) extern "C" { static LoriModuleInfo MODULE_INFO_SYMBOL __attribute__((used)) = { \
+    .name = (mname), \
+    .description = (mdescription), \
+    .init = (minit), \
+    .exit = (mexit) \
+}; }
