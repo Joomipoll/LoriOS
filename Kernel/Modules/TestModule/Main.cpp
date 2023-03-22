4366caa2c958c6ee8f46741dcc9d4793448ebc5a
Joomipoll Deulvart <95223648+Joomipoll@users.noreply.github.com>
2023-03-22 16:54:50 +0700

    Create Main.cpp

---
+++ Kernel/Modules/TestModule/Main.cpp
@@ -0,0 +1,30 @@
+#include <Module.h>
+#include <Log.h>
+#include "Tests.h"
+
+#define TEST_COUNT 2
+Test tests[TEST_COUNT]{
+    StringTest,
+	ThreadingTest,
+};
+
+static int ModuleInit()
+{
+	Log::Info("Hello, Module World!");
+
+	for(unsigned i = 0; i < TEST_COUNT; i++)
+  {
+		int ret = tests[i]();
+		if(ret != 0)
+			Log::Info("[TestModule] Failed with code %d", ret);
+	}
+
+	return 0;
+}
+
+static int ModuleExit(){
+	Log::Info("Goodbye, Module World!");
+	return 0;
+}
+
+DECLARE_MODULE("testmodule", "Runs kernel tests.", ModuleInit, ModuleExit)
