d44cae8ebf2897b0ce21a3fc8b454443ace9a3fc
Joomipoll Deulvart <95223648+Joomipoll@users.noreply.github.com>
2023-03-23 17:09:06 +0700

    Create Service.h

---
+++ Kernel/include/Obj/Service.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <FileSystem/FsVolume.h>
+#include <Obj/KObject.h>
+#include <Obj/Interface.h>
+#include <Vector.h>
+
+class Service;
+
+class ServiceFS : public fs::FsVolume
+{
+protected:
+    friend class Service;
+    static ServiceFS* instance;
+
+public:
+    List<FancyRefPtr<Service>> services;
+    FancyRefPtr<Service> kernelService;
+
+    static void Initialize(){ instance = new ServiceFS(); }
+
+    ServiceFS();
+
+    static ServiceFS* Instance()
+    {
+        assert(instance);
+
+        return instance;
+    }
+
+    long ResolveServiceName(FancyRefPtr<Service>& ref, const char* name);
+    FancyRefPtr<Service> CreateService(const char* name);
+};
+
+class Service final : public KernelObject
+{
+    DECLARE_KOBJECT(Service);
+
+protected:
+    char* name;
+    List<FancyRefPtr<MessageInterface>> interfaces;
+
+public:
+    Service(const char* _name);
+    ~Service();
+
+    void Destroy();
+
+    long CreateInterface(FancyRefPtr<MessageInterface>& rInterface, const char* name, uint16_t msgSize);
+    long ResolveInterface(FancyRefPtr<MessageInterface>& interface, const char* name);
+
+    const char* GetName() { return name; };
+};
