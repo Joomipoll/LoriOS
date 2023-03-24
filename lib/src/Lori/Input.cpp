fa254939500eefdb2a77b3c37fa135b27a719887
Joomipoll Deulvart <95223648+Joomipoll@users.noreply.github.com>
2023-03-24 12:29:53 +0700

    Create Input.cpp

---
+++ lib/src/Lori/Input.cpp
@@ -0,0 +1,25 @@
+#include <Lori/Core/Input.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+namespace Lori
+{
+    static int keyboardFd = 0;
+    static int mouseFd = 0;
+
+    ssize_t PollKeyboard(uint8_t* buffer, size_t count)
+    {
+        if(!keyboardFd) keyboardFs = open("/dev/keyboard0", O_RDONLY);
+
+        return read(keyboardFd, buffer, count);
+    }
+    
+    int PollMouse(MousePacket& pkt)
+    {
+        if(!mouseFd) mouseFd = open("/dev/mouse0", O_RDOBLY);
+        memset(&pkt, 0, sizeof(MousePacket));
+
+        return read(mouseFd, &pkt, sizeof(MousePacket));
+    }
+}
