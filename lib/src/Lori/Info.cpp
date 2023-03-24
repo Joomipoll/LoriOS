c82241510ad5003836ebcd323c9606c6691be0d2
Joomipoll Deulvart <95223648+Joomipoll@users.noreply.github.com>
2023-03-24 12:17:49 +0700

    Create Info.cpp

---
+++ lib/src/Lori/Info.cpp
@@ -0,0 +1,12 @@
+#include <Lori/Sys/Info.h>
+#include <Lori/syscall.h>
+
+namespace Lori
+{
+    lori_sysinfo_t SysInfo()
+    {
+        lori_sysinfo_t info;
+        syscall(SYS_INFO, &info);
+        return info;
+    }
+}
