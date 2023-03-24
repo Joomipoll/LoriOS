ca3817d56159708cb5359d20084cb2a219001d28
Joomipoll Deulvart <95223648+Joomipoll@users.noreply.github.com>
2023-03-24 13:57:52 +0700

    Update Lori.cpp

--- Kernel/src/Lori.cpp
+++ Kernel/src/Lori.cpp
@@ -1,3 +1,1 @@
-namespace Lori {
-char* versionString = "Lori OS Beta x86_64, " __DATE__ " " __TIME__;
-}
+namespace Lori { char* versionString = "Lori OS v.1.0 (Beta) x86_64, " __DATE__ " " __TIME__; }
