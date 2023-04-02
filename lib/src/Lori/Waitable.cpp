c0f77ddcc6de2ae8a8980e5de53f6fec3cea256e
Dragan Đukanović <95223648+Joomipoll@users.noreply.github.com>
2023-04-02 15:51:07 +0700

    Create Waitable.cpp

---
+++ lib/src/Lori/Waitable.cpp
@@ -0,0 +1,80 @@
+#include <Lori/System/Waitable.h>
+#include <Lori/System/KernelObject.h>
+
+namespace Lori
+{
+    void Waitable::Wait(long timeout) { WaitForKernelObject(GetHandle(), timeout); }
+
+    Waitable::~Waitable()
+    {
+        for (Waiter* waiter : waiters)
+        {
+            waiter->StopWaitingOn(this);
+        }
+    }
+
+    void Waiter::WaitOn(Waitable* waitable)
+    {
+        waitingOn.push_back(waitable);
+        handles.push_back(waitable->GetHandle());
+
+        waitable->waiters.push_back(this);
+    }
+
+    void Waiter::WaitOnAll(Waitable* waitable)
+    {
+        waitingOnAll.push_back(waitable);
+        waitable->GetAllHandles(handles);
+
+        waitable->waiters.push_back(this);
+    }
+
+    void Waiter::StopWaitingOn(Waitable* waitable)
+    {
+        waitable->waiters.remove(this);
+        waitingOn.remove(waitable);
+
+        RepopulateHandles();
+    }
+
+    void Waiter::StopWaitingOnAll(Waitable* waitable)
+    {
+        waitable->waiters.remove(this);
+        waitingOnAll.remove(waitable);
+
+        RepopulateHandles();
+    }
+
+    void Waiter::Wait(long timeout)
+    {
+        if (handles.size()) WaitForKernelObject(handles.data(), handles.size(), timeout);
+    }
+
+    void Waiter::RepopulateHandles()
+    {
+        handles.clear();
+
+        for (auto& waitable : waitingOn)
+        {
+            handles.push_back(waitable->GetHandle());
+        }
+
+        for (auto& waitable : waitingOnAll)
+        {
+            waitable->GetAllHandles(handles);
+        }
+    }
+
+    Waiter::~Waiter()
+    {
+        for (auto& waitable : waitingOn)
+        {
+            waitable->waiters.remove(this);
+        }
+
+        for (auto& waitable : waitingOnAll)
+        {
+            waitable->waiters.remove(this);
+        }
+    }
+}
