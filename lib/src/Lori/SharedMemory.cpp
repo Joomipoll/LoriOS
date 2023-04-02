8547ee9eaba9c951b1a89fdcf22cad71db9b60de
Dragan Đukanović <95223648+Joomipoll@users.noreply.github.com>
2023-04-02 15:36:59 +0700

    Create SharedMemory.cpp

---
+++ lib/src/Lori/SharedMemory.cpp
@@ -0,0 +1,23 @@
+#include <Lori/Core/SharedMemory.h>
+#include <lori/syscall.h>
+#include <sys/types.h>
+
+namespace Lori
+{
+    int64_t CreateSharedMemory(uint64_t size, uint64_t flags)
+    {
+        int64_t key;
+        long ret = syscall(SYS_CREATE_SHARED_MEMORY, &key, size, flags, /*recipient*/ 0);
+        if (ret < 0) return ret;
+        return key;
+    }
+
+    void* MapSharedMemory(int64_t key)
+    {
+        volatile void* ptr;
+        syscall(SYS_MAP_SHARED_MEMORY, &ptr, key, 0);
+        return (void*)ptr;
+    }
+    long UnmapSharedMemory(void* address, int64_t key) { return syscall(SYS_UNMAP_SHARED_MEMORY, address, key); }
+    long DestroySharedMemory(int64_t key) { return syscall(SYS_DESTROY_SHARED_MEMORY, key); }
+}
