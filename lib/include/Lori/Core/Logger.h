8e13157a0f1bd56e24695e6cd8c19f64d95ed1a8
Dragan Đukanović <95223648+Joomipoll@users.noreply.github.com>
2023-04-03 22:36:43 +0700

    Create Logger.h

---
+++ lib/include/Lori/Core/Logger.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+#include <mutex>
+
+#include <Lori/Core/Format.h>
+
+namespace Lori
+{
+	namespace Logger
+	{
+		const char* GetProgramName();
+
+		template <typename... Args> inline void Debug(fmt::format_string<Args...> f, Args&&... args)
+		{
+    		fmt::print(stderr, "[{}] [Debug] ", GetProgramName());
+    		fmt::vprint(stderr, f, fmt::make_format_args(args...));
+    		fmt::print(stderr, "\n");
+		}
+
+		template <typename... Args> inline void Warning(fmt::format_string<Args...> f, Args&&... args)
+		{
+    		fmt::print(stderr, "[{}] [Warning] ", GetProgramName());
+    		fmt::vprint(stderr, f, fmt::make_format_args(args...));
+    		fmt::print(stderr, "\n");
+		}
+
+		template <typename... Args> inline void Error(fmt::format_string<Args...> f, Args&&... args)
+		{
+    		fmt::print(stderr, "[{}] [Error] ", GetProgramName());
+    		fmt::vprint(stderr, f, fmt::make_format_args(args...));
+    		fmt::print(stderr, "\n");
+		}
+	}
+}
