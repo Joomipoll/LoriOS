dc12fd45d0cc9c7de3756eb47877126cde131c9e
Dragan Đukanović <95223648+Joomipoll@users.noreply.github.com>
2023-04-16 19:58:34 +0700

    Create Format.h

---
+++ lib/include/Lori/Core/Format.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Prevent use of consteval by defining as blank
+#define FMT_CONSTEVAL
+
+#define FMT_EXCEPTIONS 0
+#define FMT_HEADER_ONLY 1
+
+#include <fmt/core.h>
+#include <fmt/format.h>
