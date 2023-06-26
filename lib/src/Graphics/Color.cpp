9690ca00f5dacf81c7a0076e6a8a8ef19eb4a36f
Dragan Đukanović <95223648+Joomipoll@users.noreply.github.com>
2023-06-26 09:44:52 +0700

    Create Color.cpp

---
+++ lib/src/Graphics/Color.cpp
@@ -0,0 +1,12 @@
+#include <Lori/Graphics/Color.h>
+
+const RGBAColor RGBAColor::white = {0xff, 0xff, 0xff, 0xff};
+const RGBAColor RGBAColor::grey = {0x80, 0x80, 0x80, 0xff};
+const RGBAColor RGBAColor::black = {0, 0, 0, 0xff};
+const RGBAColor RGBAColor::red = {0xff, 0, 0, 0xff};
+const RGBAColor RGBAColor::yellow = {0xff, 0xff, 0, 0xff};
+const RGBAColor RGBAColor::green = {0, 0xff, 0, 0xff};
+const RGBAColor RGBAColor::cyan = {0, 0xff, 0xff, 0xff};
+const RGBAColor RGBAColor::blue = {0, 0, 0xff, 0xff};
+const RGBAColor RGBAColor::magenta = {0xff, 0, 0xff, 0xff};
+const RGBAColor RGBAColor::transparent = {0, 0, 0, 0};
